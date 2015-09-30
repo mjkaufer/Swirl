b7e8ba58485285dbc77796881ded5d0cea189b4e
mjkaufer <mjkaufer@gmail.com>
2015-09-30 11:46:32 -0400

    Added configuration options, readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Swirl :cyclone:
+
+## Installation
+
+Clone the repo, and run `npm install` from the commandline.
+
+## Usage
+
+Run `node main.js`. Enter your input file's name. Then, enter the x, y, and radius of the swirl. Your file will be output as `out-filename.png`, with `filename.png` being the input file's name.
+
+## Examples
+
+![Cat Pre-Swirl](/cat.png?raw=true)
+![Cat Post-Swirl](/out-cat.png?raw=true)
+
+
+## Todo
+
+* Optimize swirling for speed
--- main.js
+++ main.js
@@ -1,113 +1,105 @@
 var fs = require('fs'),
-    PNG = require('pngjs').PNG;
+	PNG = require('pngjs').PNG,
+	readline = require('readline')
 
-fs.createReadStream('cat.png')
-    .pipe(new PNG({
-        filterType: 4
-    }))
-    .on('parsed', function() {
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
-        var pixelMap = {}
+rl.question("Input file: ", function(file){
+    fs.createReadStream(file)
+        .pipe(new PNG({
+            filterType: 4
+        }))
+        .on('parsed', function() {
 
-        var width = this.width
-        var height = this.height
+            var pixelMap = {}
 
-        var usedCoordinates = []
+            var self = this
+            var width = this.width
+            var height = this.height
 
-        for (var y = 0; y < height; y++) {
-            for (var x = 0; x < width; x++) {
-                var idx = (width * y + x) << 2;
+            var usedCoordinates = []
 
-                pixelMap[JSON.stringify({x: x, y: y})] = {
-                    r: this.data[idx],
-                    g: this.data[idx + 1],
-                    b: this.data[idx + 2]
+            for (var y = 0; y < height; y++) {
+                for (var x = 0; x < width; x++) {
+                    var idx = (width * y + x) << 2;
+
+                    pixelMap[JSON.stringify({x: x, y: y})] = {
+                        r: this.data[idx],
+                        g: this.data[idx + 1],
+                        b: this.data[idx + 2]
+                    }
+                    // // and reduce opacity
+                    // this.data[idx+3] = this.data[idx+3] >> 1;
                 }
-                // // and reduce opacity
-                // this.data[idx+3] = this.data[idx+3] >> 1;
             }
-        }
+
+            rl.question("x position: ", function(x0){
+                rl.question("y position: ", function(y0){
+                    rl.question("radius: ", function(maxRadius){
+                        x0 = parseInt(x0)
+                        y0 = parseInt(y0)
+                        maxRadius = parseInt(maxRadius)
 
 
-        var maxRadius = 100
-        for(var r = 0 ; r < maxRadius; r++)
-            cycle(width/2, height/2, maxRadius - r, parseInt(r / 2))    
+                        for(var r = 0 ; r < maxRadius; r++)
+                            cycle(x0, y0, maxRadius - r, parseInt(r / 2))    
 
-        
+                        for (var y = 0; y < height; y++) {
+                            for (var x = 0; x < width; x++) {
+                                var idx = (width * y + x) << 2;
 
+                                var pixel = pixelMap[JSON.stringify({x: x, y: y})]
+                                self.data[idx] = pixel.r;
+                                self.data[idx+1] = pixel.g;
+                                self.data[idx+2] = pixel.b;
+                                // console.log(this.data[idx], this.data[idx + 1], this.data[idx + 2])
+                            }
+                        }
 
-        for (var y = 0; y < height; y++) {
-            for (var x = 0; x < width; x++) {
-                var idx = (width * y + x) << 2;
-
-                var pixel = pixelMap[JSON.stringify({x: x, y: y})]
-                this.data[idx] = pixel.r;
-                this.data[idx+1] = pixel.g;
-                this.data[idx+2] = pixel.b;
-                // console.log(this.data[idx], this.data[idx + 1], this.data[idx + 2])
-            }
-        }
-
-
-
-        function cycle(startX, startY, radius, shiftAmount){
-            console.log("Cycling at (", startX, ",", startY, ") with radius", radius, "by shift", shiftAmount)
-            var coordinates = []
-
-            // console.log("ay")
-            // for(var y = 0; y < height; y++){
-            //     for(var x = 0; x < width; x++){//maybe this isn't the most effective way (ok, I'm positive it isn't), but I'm on a time budget
-            //         var d = Math.sqrt(Math.pow(startX - x, 2) + Math.pow(startY - y, 2))
-            //         var positionString = JSON.stringify({x: x, y: y})
-
-            //         if(Math.abs(radius - d) < 0.5 && usedCoordinates.indexOf(positionString) == -1){
-            //             coordinates.push({x: x, y: y})
+                        self.pack().pipe(fs.createWriteStream('out-' + file));
+                        rl.close()
                         
-            //         }
-                    
-            //         console.log((x + y * width),"/", (height * width))
-
-            //         usedCoordinates.indexOf(positionString) == -1 && usedCoordinates.push(positionString)
-            //     }
-            // }
-
-            for(var theta = 0; theta < 360; theta += 0.25){
-                var thetaRad = Math.PI * theta / 180
-                var x = parseInt(Math.cos(thetaRad) * radius + startX)
-                var y = parseInt(Math.sin(thetaRad) * radius + startY)
-
-                var positionString = JSON.stringify({x: x, y: y})
-
-                usedCoordinates.indexOf(positionString) == -1 && coordinates.push(positionString) && usedCoordinates.push(positionString)// && console.log(pixelMap[{x: x, y: y}])
-
-            }
-            
-
-            var values = []
-
-            for(var i = 0; i < coordinates.length; i++){
-                var rgb = pixelMap[coordinates[i]]
-                values.push(JSON.stringify(rgb))
-                // console.log(values[i])
-            }
-
-            for(var i = 0; i < coordinates.length; i++){
-
-                pixelMap[coordinates[i]] = JSON.parse(values[(i + shiftAmount) % values.length])
-                // for(var key in pixelMap[coordinates[i]])
-                //     pixelMap[coordinates[i]][key] = 255-pixelMap[coordinates[i]][key]
+                    })
+                })
+            })
 
 
-            }
 
 
-            
 
+            function cycle(startX, startY, radius, shiftAmount){
+                console.log("Cycling at (", startX, ",", startY, ") with radius", radius, "by shift", shiftAmount)
+                var coordinates = []
 
-            
-        }
+                for(var theta = 0; theta < 360; theta += 0.25){
+                    var thetaRad = Math.PI * theta / 180
+                    var x = parseInt(Math.cos(thetaRad) * radius + startX)
+                    var y = parseInt(Math.sin(thetaRad) * radius + startY)
 
-        
+                    var positionString = JSON.stringify({x: x, y: y})
 
-        this.pack().pipe(fs.createWriteStream('out.png'));
-    });
+                    usedCoordinates.indexOf(positionString) == -1 && coordinates.push(positionString) && usedCoordinates.push(positionString)// && console.log(pixelMap[{x: x, y: y}])
+
+                }
+                
+
+                var values = []
+
+                for(var i = 0; i < coordinates.length; i++){
+                    var rgb = pixelMap[coordinates[i]]
+                    values.push(JSON.stringify(rgb))
+                    // console.log(values[i])
+                }
+
+                for(var i = 0; i < coordinates.length; i++)
+                    pixelMap[coordinates[i]] = JSON.parse(values[(i + shiftAmount) % values.length])
+
+            }            
+
+        });    
+    // rl.close()
+})
+
